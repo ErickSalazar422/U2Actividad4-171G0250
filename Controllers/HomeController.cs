1630107aaac5c25c46b035d3bfe087d029d5855f
ErickSalazar422 <71106495+ErickSalazar422@users.noreply.github.com>
2020-10-05 21:54:29 -0500

    fainaly

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,19 +17,7 @@ namespace U2Actividad5_171G0250.Controllers
            
             animalesContext context = new animalesContext();
           var clas = context.Clase.OrderBy(x=>x.Nombre);
-            //var clas = context.Clase.FirstOrDefault(x => x.Nombre.ToUpper() == nombre);
-
-
-            //ClaseAnViewModel vm = new ClaseAnViewModel();
-            //vm.Clase = clas;
-            //Random r = new Random();
-            //vm.otrita = context.Clase.Where(x => x.Id==clas.Id).OrderBy(x => r.Next(0, 5)).Take(1);
-            //vm.clasesita = clas.Especies.OrderBy(x => x.IdClaseNavigation.Nombre)
-            //        .GroupBy(x => x.IdClaseNavigation.Nombre).Select(x => x).ToArray();
-
-
-
-            //var random = context.Clase.Select(x => x.Id).OrderBy(x => r.Next(0, 5)).Take(1);
+           
 
             return View(clas);
         }
@@ -38,8 +26,7 @@ namespace U2Actividad5_171G0250.Controllers
         {
             var nombre = id;
             animalesContext context = new animalesContext();
-            //var clasif = context.Especies.Where(x => x.IdClaseNavigation.Nombre.ToUpper() == id)
-            //    .OrderBy(x => x.Especie);
+            
 
             var idesp = context.Especies.OrderBy(x => x.Id);
             var Clas = context.Clase.Include(x => x.Especies).ThenInclude(x => x.IdClaseNavigation).FirstOrDefault(x => x.Nombre.ToUpper() == nombre);
